690aeebb372c07c967a1ae00d4eba34c8b37fe22
Johannes Millan <johannes.millan@gmail.com>
2015-01-07 21:14:34 +0100

    better uuid (no dashes)

--- src/form-d.js
+++ src/form-d.js
@@ -32,7 +32,7 @@ angular.module('ngFabForm')
         {
             /* jshint ignore:start */
             var d = new Date().getTime();
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c)
+            return 'xxxxxxxx_xxxx_4xxx_yxxx_xxxxxxxxxxxx'.replace(/[xy]/g, function (c)
             {
                 var r = (d + Math.random() * 16) % 16 | 0;
                 d = Math.floor(d / 16);
