77c5f7aabb861d51a2c865d8890ef04b83311d51
Johannes Millan <johannes.millan@gmail.com>
2015-02-16 21:19:22 +0100

    Updates

--- ng-fab-form.js
+++ ng-fab-form.js
@@ -208,60 +208,6 @@ angular.module('ngFabForm')
     });
 
 angular.module('ngFabForm')
-    .directive('maxFileSize', function (maxUploadSizeInByte)
-    {
-        'use strict';
-
-        var config = {
-            maxUploadSizeInByte: maxUploadSizeInByte,
-            validationKey: 'maxFileSize'
-        };
-
-        return {
-            restrict: 'A',
-            require: '?ngModel',
-            scope: {
-                'ngModel': '=',
-                'maxFileSize': '@'
-            },
-            link: function (scope, el, attrs, ngModel)
-            {
-                var maxFileSize;
-
-                // only assign once
-                if (!scope.maxFileSize || parseFloat(scope.maxFileSize) > 0) {
-                    maxFileSize = config.maxUploadSizeInByte;
-                } else {
-                    maxFileSize = scope.maxFileSize;
-                }
-
-                scope.$watch('ngModel', function (newVal)
-                {
-                    if (newVal instanceof Array) {
-                        ngModel.$setViewValue(newVal);
-
-                        // if one of the selected files is bigger than allowed
-                        // set validation-status to false
-                        var isValid = true;
-                        for (var i = 0; i < scope.ngModel.length; i++) {
-                            var file = scope.ngModel[i];
-                            if (file.size > maxFileSize) {
-                                isValid = false;
-                                break;
-                            }
-                        }
-                        ngModel.$setValidity(config.validationKey, isValid);
-                    } else {
-                        ngModel.$setValidity(config.validationKey, true);
-                    }
-
-                    // set third $watch param to true for object equality
-                }, true);
-            }
-        };
-    });
-
-angular.module('ngFabForm')
     .directive('input', function (ngFabFormValidationsDirective)
     {
         'use strict';
@@ -555,7 +501,7 @@ angular.module('ngFabForm')
 
             // remove error tpl if any
             if (params.currentValidationVars.tpl && (Object.keys(params.currentValidationVars.tpl).length !== 0)) {
-                params.currentValidationVars.tpl.remove();
+                angular.element(params.currentValidationVars.tpl).remove();
             }
 
             // load validation directive template
@@ -646,7 +592,7 @@ angular.module('ngFabForm')
                         }
                         // otherwise remove if a tpl was set before
                         else if (!cfg.validationsTemplate && currentValidationVars.tpl && (Object.keys(currentValidationVars.tpl).length !== 0)) {
-                            currentValidationVars.tpl.remove();
+                            angular.element(currentValidationVars.tpl).remove();
                         }
 
                         // set asterisk for labels
