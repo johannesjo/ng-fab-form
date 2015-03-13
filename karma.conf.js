031f9f2c1958a3592218ba10297f82601c9dffbe
Johannes Millan <johannes.millan@gmail.com>
2015-03-13 20:59:00 +0100

    exclude spec files from coverage

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config)
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            'src/*.js': ['coverage']
+            '**/src/**/!(*.spec)+(.js)': ['coverage']
         },
 
         // web server port
