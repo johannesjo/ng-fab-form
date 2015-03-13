78f58a3283e9f2f44abc2375f5446e53f2a43452
Johannes Millan <johannes.millan@gmail.com>
2015-03-13 21:20:27 +0100

    Release v1.2.1

--- dist/ng-fab-form.js
+++ dist/ng-fab-form.js
@@ -438,47 +438,7 @@ angular.module('ngFabForm').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('default-validation-msgs.html',
-    "<div ng-messages=\"field.$error\"\n" +
-    "     class=\"validation\"\n" +
-    "     ng-show=\"attrs.required==''|| attrs.required\">\n" +
-    "    <ul class=\"list-unstyled validation-errors\"\n" +
-    "        ng-show=\"field.$invalid && (field.$touched || field.$dirty || form.$triedSubmit)\">\n" +
-    "        <li ng-message=\"required\">This field is required</li>\n" +
-    "        <li ng-message=\"password\">Please enter a valid password\n" +
-    "        </li>\n" +
-    "        <li ng-message=\"email\">Please enter a valid e-mail</li>\n" +
-    "        <li ng-message=\"pattern\">Invalid input format</li>\n" +
-    "        <li ng-message=\"date\">Please enter a valid date</li>\n" +
-    "        <li ng-message=\"time\">Please enter a valid time</li>\n" +
-    "        <li ng-message=\"datetime\">Please enter a valid date and time</li>\n" +
-    "        <li ng-message=\"datetime-local\">Please enter a valid date and time</li>\n" +
-    "        <li ng-message=\"number\">This field must be numeric</li>\n" +
-    "        <li ng-message=\"color\">Please enter a valid color</li>\n" +
-    "        <li ng-message=\"range\">Please enter a valid range</li>\n" +
-    "        <li ng-message=\"month\">Please enter a valid month</li>\n" +
-    "        <li ng-message=\"url\">Please enter a valid URL</li>\n" +
-    "        <li ng-message=\"file\">Invalid file</li>\n" +
-    "        <li ng-message=\"minlength\">Please use at least {{ attrs.minlength }} characters</li>\n" +
-    "        <li ng-message=\"maxlength\">Please do not exceed {{ attrs.maxlength }} characters</li>\n" +
-    "        <li ng-message=\"match\">The {{ attrs.type ==='password'? 'passwords' : 'values' }} should match</li>\n" +
-    "        <li ng-if=\"attrs.type == 'time' \"\n" +
-    "            ng-message=\"min\">The time provided should after {{ attrs.min |date: 'HH:MM' }}\n" +
-    "        </li>\n" +
-    "        <li ng-message=\"max\"\n" +
-    "            ng-if=\"attrs.type == 'time' \">The time provided should be before {{attrs.max |date: 'HH:MM'}}\n" +
-    "        </li>\n" +
-    "        <li ng-message=\"min\"\n" +
-    "            ng-if=\"attrs.type == 'date' \">The date provided should be after {{attrs.min\n" +
-    "            |date:'dd.MM.yy'}}\n" +
-    "        </li>\n" +
-    "        <li ng-message=\"max\"\n" +
-    "            ng-if=\"attrs.type == 'date' \">The date provided should be before {{attrs.max |date: 'dd.MM.yy'}}\n" +
-    "        </li>\n" +
-    "    </ul>\n" +
-    "    <div class=\"validation-success\"\n" +
-    "         ng-show=\"field.$valid && !field.$invalid\">\n" +
-    "    </div>\n" +
-    "</div>\n"
+    "<div ng-messages=\"field.$error\" class=\"validation\" ng-show=\"attrs.required==''|| attrs.required\"><ul class=\"list-unstyled validation-errors\" ng-show=\"field.$invalid && (field.$touched || field.$dirty || form.$triedSubmit)\"><li ng-message=\"required\">This field is required</li><li ng-message=\"password\">Please enter a valid password</li><li ng-message=\"email\">Please enter a valid e-mail</li><li ng-message=\"pattern\">Invalid input format</li><li ng-message=\"date\">Please enter a valid date</li><li ng-message=\"time\">Please enter a valid time</li><li ng-message=\"datetime\">Please enter a valid date and time</li><li ng-message=\"datetime-local\">Please enter a valid date and time</li><li ng-message=\"number\">This field must be numeric</li><li ng-message=\"color\">Please enter a valid color</li><li ng-message=\"range\">Please enter a valid range</li><li ng-message=\"month\">Please enter a valid month</li><li ng-message=\"url\">Please enter a valid URL</li><li ng-message=\"file\">Invalid file</li><li ng-message=\"minlength\">Please use at least {{ attrs.minlength }} characters</li><li ng-message=\"maxlength\">Please do not exceed {{ attrs.maxlength }} characters</li><li ng-message=\"match\">The {{ attrs.type ==='password'? 'passwords' : 'values' }} should match</li><li ng-if=\"attrs.type == 'time' \" ng-message=\"min\">The time provided should after {{ attrs.min |date: 'HH:MM' }}</li><li ng-message=\"max\" ng-if=\"attrs.type == 'time' \">The time provided should be before {{attrs.max |date: 'HH:MM'}}</li><li ng-message=\"min\" ng-if=\"attrs.type == 'date' \">The date provided should be after {{attrs.min |date:'dd.MM.yy'}}</li><li ng-message=\"max\" ng-if=\"attrs.type == 'date' \">The date provided should be before {{attrs.max |date: 'dd.MM.yy'}}</li></ul><div class=\"validation-success\" ng-show=\"field.$valid && !field.$invalid\"></div></div>"
   );
 
 }]);
