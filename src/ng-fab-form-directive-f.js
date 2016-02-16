abdf0ba85626ff1d0176a5afe2d1c0456e61ad1a
Johannes Millan <johannes.millan@gmail.com>
2016-02-16 00:35:45 +0000

    optionally require form controller to prevent conflicts with formly

--- src/ng-fab-form-directive-f.js
+++ src/ng-fab-form-directive-f.js
@@ -46,7 +46,7 @@ angular.module('ngFabForm')
         return {
             restrict: 'EAC',
             scope: false,
-            require: 'form',
+            require: '?^form',
             compile: function (el, attrs)
             {
                 // create copy of configuration object as it might be modified by ngFabFormOptions
